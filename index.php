8f3eb112f55d6eab4564fff5ce39def8fd63984c
akpoflash <akpoflash@gmail.com>
2016-09-30 18:53:42 +0300

    Add attribute defer and include scripts on top.

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
         <title>Грузоперевозки на газели в Туле</title>
         <link rel="stylesheet" href="css/style.min.css">
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+        <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter34912640 = new Ya.Metrika({ id:34912640, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/34912640" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+        <script src="js/jquery.min.js" defer></script>
+        <script src="js/script.min.js" defer></script>
     </head>
     <body>
       <div class="ajax-alert-popup">
@@ -204,8 +207,5 @@
                 </div>
             </footer>
         </div>
-        <script src="js/jquery.min.js"></script>
-        <script src="js/script.min.js"></script>
-        <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter34912640 = new Ya.Metrika({ id:34912640, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/34912640" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
     </body>
 </html>
